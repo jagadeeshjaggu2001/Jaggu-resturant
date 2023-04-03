539d9a31676a46485088f13a69b13bc80a73e4db
jagadeeshjaggu2001 <jagadeeshjaggu002@gmail.com>
2023-04-03 15:05:15 +0530

    first commit

---
+++ resturant.js
@@ -0,0 +1,191 @@
+const menu = [
+    {
+        id: 1,
+        title: "Chicken burger",
+        category: "breakfast",
+        price: "$20",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz8nHi3FAqh1Ez5aS6k5C0iAVfch9XjYStQw&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 2,
+        title: "milkshakes",
+        category: "shakes",
+        price: "$10",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt6QO_q2oLVSMlcg9F86cM4QXVlwJhhT835g&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 3,
+        title: "Pancakes",
+        category: "snack",
+        price: "$15",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlos0pGNPJSauiBvbaoQTzZjPw5sYPYG7Tgw&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 4,
+        title: "Special Dishes",
+        category: "lunch",
+        price: "$30",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9mQrJXBhMoip2sgvtPajvAAoP_pXLDsuNeImOp-BR5hqZ-hKuWk83cZFu7Du-ix570jA&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 5,
+        title: "Dinner Double",
+        category: "dinner",
+        price: "$33",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzeLp3w7bvGVJATF8W81x_F5--Qg0ZX9iw9A&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 6,
+        title: "Special Dishes",
+        category: "lunch",
+        price: "$25",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK62SVYjevHDhiIfUcr1gi_1Sq9BbMzotK8Q&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 7,
+        title: "Special Dishes",
+        category: "lunch",
+        price: "$40",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3ef2L_x-oNzWynP1x4I87l50Rve5S30nRqQ&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 8,
+        title: "milkshakes",
+        category: "shakes",
+        price: "$15",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8ldthbAcggQS5vIw8JsxYoqlwnNq6rFZ2fQ&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 9,
+        title: "milkshakes",
+        category: "shakes",
+        price: "$12",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPaGb_apwbnQbmAXm3lFUae6aCt0CyYvKk2A&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 10,
+        title: "Pancakes",
+        category: "snack",
+        price: "$15",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSYseE1APFiCAJQ9svR4A2il5GvrSIdr-GHw&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 11,
+        title: "Pancakes",
+        category: "snack",
+        price: "$15",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrAyP2lkQfGDdaKm2W2UhK3sBNVV6jH_DtuQ&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 12,
+        title: "Dinner Double",
+        category: "dinner",
+        price: "$30",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbkHtVa_3wOiEscmCcqVNH1Kv1lAujxU-2rQ&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 13,
+        title: "Dinner Double",
+        category: "dinner",
+        price: "$35",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIrfppxwPASsO78bspTwbA8rWP6tvHL0o6mQ&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 14,
+        title: "Chicken burger",
+        category: "breakfast",
+        price: "$20",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIVfrq4VmmJPEKRHIy8NWefa_t0urRzLy0GQ&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    {
+        id: 15,
+        title: "Chicken burger",
+        category: "breakfast",
+        price: "$25",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu1OqwiIBrit2IJ_3BCGMKGKzax28XD9dRKw&usqp=CAU",
+        desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold pressed`
+
+    },
+    
+
+
+
+];
+
+const sectionCenter = document.querySelector(".section-center");
+
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+window.addEventListener("DOMContentLoaded", function () {
+    displayItems(menu);
+});
+
+filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function(e) {
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function(menuItems){
+            if(menuItems.category === category){
+                return menuItems;
+            }
+
+        });
+
+        // console.log(menuCategory);
+        if(category == 'all'){
+            displayItems(menu);
+        }
+        else{
+            displayItems(menuCategory);
+        }
+    });
+});
+
+function displayItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+        console.log(item);
+
+        return ` <article class="menuitem">
+        <img src=${item.img}
+            alt=${item.title} class="burg">
+    </article>
+    <div class="item-info">
+        <header>
+            <h3>${item.title}</h3>
+            <h4 class="price">${item.price} </h4>
+        </header>
+        <p class="item-text">${item.desc} </p>
+    </div>`;
+    });
+
+    displayMenu = displayMenu.join("");
+    sectionCenter.innerHTML = displayMenu;
+}
+
